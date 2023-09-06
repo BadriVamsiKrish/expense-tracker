80c0da99d31fbdb4549434772001c405812eb96b
BadriVamsiKrish <www.1203badrivamsi@.com>
2023-09-06 12:11:45 +0530

    task 9

--- src/pages/Expenceform.js
+++ src/pages/Expenceform.js
@@ -1,16 +1,38 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button } from 'react-bootstrap';
-
+import axios from 'axios';
 const Expenceform = () => {
+  const[datalist,setDatalist]=useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData(); // Call the function to fetch data when the component mounts
+  }, []);
+
+  const fetchData = () => {
+    axios.get('https://react-http-f171e-default-rtdb.firebaseio.com/expence_cart.json')
+      .then((response) => {
+        const fetchedData = response.data;
+        setData(fetchedData);
+        //setDatalist(fetchedData);// Update the component state with the data
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  };
   const money=useRef();
   const[moneyval,setMoneyval]=useState('');
   const description=useRef();
   const[descriptionval,setDescriptionval]=useState('');
   const category=useRef();
   const[categoryval,setCategoryval]=useState('');
-  const[datalist,setDatalist]=useState([]);
+  
   const addhandler = () =>{
+    
     datalist.push({'money':parseInt(money.current.value),'description':description.current.value,'category':category.current.value});
+    // Make a POST request using fetch
+    axios.post('https://react-http-f171e-default-rtdb.firebaseio.com/expence_cart.json',{'money':parseInt(money.current.value),'description':description.current.value,'category':category.current.value}).then(()=>alert('data is submited')).catch((er)=>alert(er));
+
     setMoneyval('');
     setDescriptionval('');
     setCategoryval('');
@@ -25,8 +47,13 @@ const Expenceform = () => {
         Category:<input value={categoryval} type='text'onChange={(event)=>setCategoryval(event.target.value)} ref={category}/><br/>
         <Button style={{alignSelf:'flex-end'}} onClick={(event)=>addhandler()}>Add</Button>
         <h2>Items</h2>
-        <ol>{datalist.map((item)=>{return (<li key={Math.random()}>Money:{item.money}-description:{item.description}-category:{item.category}</li>)})}</ol>
-
+        <ol>{Object.keys(data).map((key) => (
+          <li key={key}>
+          <p>Category: {data[key].category}</p>
+          <p>Description: {data[key].description}</p>
+          <p>Money: {data[key].money}</p>
+        </li>
+      ))}</ol>
       </form>
     </div>
   )
